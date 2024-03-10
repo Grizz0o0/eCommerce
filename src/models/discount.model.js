30fd87817c50647887fd6527f55d682e6f75274e
Hong Ky <vuonghongky26@gmail.com>
2024-03-10 22:51:52 +0700

    discount model

---
+++ src/models/discount.model.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { Schema, model } = require('mongoose'); // Erase if already required
+
+const DOCUMENT_NAME = 'Discount';
+const COLLECTION_NAME = 'Discounts';
+
+// Declare the Schema of the Mongo model
+var discountSchema = new Schema(
+    {
+        discount_name: { type: String, required: true },
+        discount_description: { type: String, required: true },
+        discount_type: { type: String, default: 'fixed_amount' }, // percentage
+        discount_value: { type: Number, required: true }, // 10.000  10
+        discount_code: { type: String, required: true }, // discountCode
+        discount_start_day: { type: Date, required: true }, // ngay bat dau
+        discount_end_date: { type: String, required: true }, // ngay ket thuc
+        discount_max_uses: { type: Number, required: true }, // so luong discount duoc ap dung
+        discount_uses_count: { type: Number, required: true }, // so discount da su dung
+        discount_uses_used: { type: Array, default: [] }, // ai da dung
+        discount_max_uses_per_user: { type: Number, required: true }, // so luong cho phep toi da moi user
+        discount_min_order_value: { type: Number, required: true },
+        discount_shopId: { type: Schema.Types.ObjectId, ref: 'Shop' },
+        discount_is_active: { type: Boolean, default: true },
+        discount_applies_to: {
+            type: String,
+            required: true,
+            enum: ['all', 'specific'],
+        },
+        discount_product_ids: { type: Array, default: [] }, // so san pham dc ap dung
+    },
+    {
+        collection: COLLECTION_NAME,
+        timestamps: true,
+    }
+);
+
+//Export the model
+module.exports = model(DOCUMENT_NAME, discountSchema);
